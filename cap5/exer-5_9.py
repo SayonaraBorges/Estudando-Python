0292c8cf66d3647e719b2b80a45cc58032e6b031
Sayonara Borges <sayonborges@gmail.com>
2024-05-30 11:02:30 -0300

    pequenas correções na revisao do código.

--- cap5/exer-5_9.py
+++ cap5/exer-5_9.py
@@ -1,5 +1,5 @@
 current_users = ['Alexandre', 'Samara', 'Natã', 'Sabrina', 'Sandra', 'Júnior']
-new_users = ['alexandre', 'Samara', 'Maria', 'Luana', 'João', 'Wagner']
+new_users = ['Alexandre', 'Samara', 'Maria', 'Luana', 'João', 'Wagner']
 
 for user in new_users:
     if user.lower() in [u.lower() for u in current_users]:
--- cap6/exer-6_8.py
+++ cap6/exer-6_8.py
@@ -28,4 +28,4 @@ for pet in pets:
    nome = pet.get('nome')
    tipo = pet.get('tipo')
    especie = pet.get('é')
-   print(f"O animal de estimação de {dono.title()} é um {especie} {tipo}, seu nome é {nome.title()}.")
+   print(f"O animal de estimação de {dono.title()} é um {especie}, do tipo {tipo}, seu nome é {nome.title()}.")
