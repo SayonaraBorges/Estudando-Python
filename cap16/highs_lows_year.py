4cadc28e9319ebb49d165a1b58cd5f0e9a0c246b
Sayonara Borges <sayonborges@gmail.com>
2024-06-16 18:05:00 -0300

    estudos do cap16

--- cap16/highs_lows_year.py
+++ cap16/highs_lows_year.py
@@ -1,12 +1,12 @@
 import csv
 from matplotlib import pyplot as plt
 from datetime import datetime
-# Obtém as datas e as temperaturas máximas do arquivo
-filename = r"H:\\Meu Drive\python_work\Estudando-Python\cap16\csvfiles\sitka_weather_07-2014.csv"
+# Obtém as datas e as temperaturas máximas e mínimas do arquivo
+filename = r"H:\\Meu Drive\python_work\Estudando-Python\cap16\csvfiles\sitka_weather_2014.csv"
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    dates, highs = [], []
+    dates, highs, lows = [], [], []
     #for index, column_header in enumerate(header_row):
     #    print(index, column_header)
     for row in reader:
@@ -14,13 +14,16 @@ with open(filename) as f:
         dates.append(current_date)
         high = int(row[1])
         highs.append(high)
-    #print(highs)
+        low = int(row[3])
+        lows.append(low)
 
     #Faz a plotagem dos dados 
     fig = plt.figure(dpi=128, figsize=(10,6))
-    plt.plot(dates, highs, c='red')
+    plt.plot(dates, highs, c='red', alpha=0.5)
+    plt.plot(dates, lows, c='blue', alpha=0.5)
+    plt.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
     #Formata o gráfico
-    plt.title("Daily high temperatures, July 2014", fontsize=24)
+    plt.title("Daily high and low temperatures - 2014", fontsize=24)
     plt.xlabel('', fontsize=16)
     fig.autofmt_xdate()
     plt.ylabel('Temperature (F)', fontsize=16)
---
+++ cap16/world_population.py
@@ -0,0 +1,12 @@
+import json
+
+# Carrega os dados em uma lista
+filename = r'H:\\Meu Drive\python_work\Estudando-Python\cap16\csvfiles\population_data.json'
+with open(filename) as f:
+    pop_data = json.load(f)
+# Exibe a população de cada país em 2010
+for pop_dict in pop_data:
+    if pop_dict['Year'] == '2010':
+        country_name = pop_dict['Country Name'] 
+        population = pop_dict['Value'] 
+        print(country_name + ": " + population)
